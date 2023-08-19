4f1a6e184f3dd700416bfcd131a2af88c052ecc3
Elias Kekana <thapeloelias.kekana@gmail.com>
2023-08-19 15:57:03 +0200

    chore: add service locator and add a barrel file in domain usecases

---
+++ lib/features/stopwatch/domain/usecases/domain_usecases.dart
@@ -0,0 +1,6 @@
+export 'add_lap_usecase.dart';
+export 'get_activity_history_usecase.dart';
+export 'get_stopwatch_usecase.dart';
+export 'pause_stopwatch_usecase.dart';
+export 'reset_stopwatch_usecase.dart';
+export 'start_stopwatch_usecase.dart';
